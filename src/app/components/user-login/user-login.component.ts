925413f7299307d4b9fc957de599d89ca769a082
theodvar <vtheodosia@gmail.com>
2024-04-23 19:29:04 +0300

    Bug fixes

--- src/app/components/user-login/user-login.component.ts
+++ src/app/components/user-login/user-login.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { jwtDecode } from 'jwt-decode';
 import { Credentials, LoggedInUser } from 'src/app/shared/interfaces/user';
 import { UserService } from 'src/app/shared/services/user.service';
@@ -8,7 +8,7 @@ import { UserService } from 'src/app/shared/services/user.service';
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
